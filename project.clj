ef6add4e6d96c920e8015bb2b09b0f3594b10359
Travis Vachon <travis@copious.com>
2012-05-22 13:36:31 -0400

    lein-release plugin: preparing 1.0.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.utahstreetlabs/lein-release "1.0.1-SNAPSHOT"
+(defproject com.utahstreetlabs/lein-release "1.0.1"
   :description "Leiningen Release Plugin"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in :leiningen
